8f41202e05e298d2de7153f5d79baa78dea9e056
Kaname Kizu <kaname.kizz@gmail.com>
2015-05-23 23:30:38 +0900

    Add tests and fix bugs of numberToString().

--- test/autogen-numberToString.spec.js
+++ test/autogen-numberToString.spec.js
@@ -80,6 +80,76 @@ describe("numberToString", function () {
     expect(actual).equal(" 2a   ");
   });
 
+  it("[\"42\",42.63,null,1,null,\"d\"]", function () {
+    var actual = numberToString(42.63, undefined, 1, undefined, "d");
+    expect(actual).equal("42");
+  });
+
+  it("[\"     42\",42.63,null,7,null,\"d\"]", function () {
+    var actual = numberToString(42.63, undefined, 7, undefined, "d");
+    expect(actual).equal("     42");
+  });
+
+  it("[\"      42\",42.63,null,8,null,\"d\"]", function () {
+    var actual = numberToString(42.63, undefined, 8, undefined, "d");
+    expect(actual).equal("      42");
+  });
+
+  it("[\"      42\",42.63,null,8,0,\"d\"]", function () {
+    var actual = numberToString(42.63, undefined, 8, 0, "d");
+    expect(actual).equal("      42");
+  });
+
+  it("[\"      42\",42.63,null,8,2,\"d\"]", function () {
+    var actual = numberToString(42.63, undefined, 8, 2, "d");
+    expect(actual).equal("      42");
+  });
+
+  it("[\"42.63\",42.63,null,null,null,\"f\"]", function () {
+    var actual = numberToString(42.63, undefined, undefined, undefined, "f");
+    expect(actual).equal("42.63");
+  });
+
+  it("[\"42.63\",42.63,null,4,null,\"f\"]", function () {
+    var actual = numberToString(42.63, undefined, 4, undefined, "f");
+    expect(actual).equal("42.63");
+  });
+
+  it("[\"42.63\",42.63,null,5,null,\"f\"]", function () {
+    var actual = numberToString(42.63, undefined, 5, undefined, "f");
+    expect(actual).equal("42.63");
+  });
+
+  it("[\" 42.63\",42.63,null,6,null,\"f\"]", function () {
+    var actual = numberToString(42.63, undefined, 6, undefined, "f");
+    expect(actual).equal(" 42.63");
+  });
+
+  it("[\"  42.63\",42.63,null,7,null,\"f\"]", function () {
+    var actual = numberToString(42.63, undefined, 7, undefined, "f");
+    expect(actual).equal("  42.63");
+  });
+
+  it("[\"   42.63\",42.63,null,8,null,\"f\"]", function () {
+    var actual = numberToString(42.63, undefined, 8, undefined, "f");
+    expect(actual).equal("   42.63");
+  });
+
+  it("[\"      43\",42.63,null,8,0,\"f\"]", function () {
+    var actual = numberToString(42.63, undefined, 8, 0, "f");
+    expect(actual).equal("      43");
+  });
+
+  it("[\"    42.6\",42.63,null,8,1,\"f\"]", function () {
+    var actual = numberToString(42.63, undefined, 8, 1, "f");
+    expect(actual).equal("    42.6");
+  });
+
+  it("[\"   42.63\",42.63,null,8,2,\"f\"]", function () {
+    var actual = numberToString(42.63, undefined, 8, 2, "f");
+    expect(actual).equal("   42.63");
+  });
+
   it("[\"42\",42,null,null,null,\"d\"]", function () {
     var actual = numberToString(42, undefined, undefined, undefined, "d");
     expect(actual).equal("42");
